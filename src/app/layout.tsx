2bb5b386e4922c3da872300dd706e7475a445f74
rickytabe <rickytabe2@gmail.com>
2026-07-04 00:35:03 +0100

    feat: initialize root layout with custom fonts, theme provider, metadata, and schema markup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,11 +54,20 @@ export const metadata: Metadata = {
     siteName: "Tabe Rickson Portfolio",
     type: "website",
     locale: "en_US",
+    images: [
+      {
+        url: "/opengraph-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Tabe Rickson Portfolio",
+      },
+    ],
   },
   twitter: {
     card: "summary_large_image",
     title,
     description,
+    images: ["/twitter-image.png"],
   },
 };
 
